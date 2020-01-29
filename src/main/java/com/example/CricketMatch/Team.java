35abc8ac4d6b6f5ab2e1fccce289f96e75e2f888
gge201 <59999590+gge201@users.noreply.github.com>
2020-01-29 09:49:26 +0530

    Add files via upload

---
+++ src/main/java/com/example/CricketMatch/Team.java
@@ -0,0 +1,28 @@
+package com.example.CricketMatch;
+
+public class Team {
+    Player P[]=new Player[11];
+    private int overs;
+    private String teamName;
+
+    public int getOvers() {
+        return overs;
+    }
+
+    public void setOvers(int overs) {
+        this.overs = overs;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    Team(String name, int overs) {
+        teamName=name;
+        this.overs=overs;
+    }
+}
